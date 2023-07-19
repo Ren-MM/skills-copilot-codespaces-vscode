c5d093863ddbc18927de1ce75383b549c8c11981
Ren Munro <95164259+Ren-MM@users.noreply.github.com>
2023-07-19 14:33:04 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create a web server
+// 1. Initialize express
+// 2. Initialize the route
+// 3. Listen to the port
+
+// 1. Initialize express
+const express = require('express');
+const app = express();
+
+// 2. Initialize the route
+const comments = require('./routes/comments');
+app.use('/comments', comments);
+
+// 3. Listen to the port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port ${port}`));
